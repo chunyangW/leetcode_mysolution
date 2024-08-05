e3f9666c925107dfa6d27023cb8cf8c22b2215f4
cy <1790584259@qq.com>
2024-08-05 16:08:05 +0800

    8.5

--- py_solution/test.py
+++ py_solution/test.py
@@ -1,30 +1,51 @@
 ## just for test
+## 从头手搓
 from typing import List
 
-class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        lens = len(nums)
-        i = j = 0
-        while(1):
-            while(nums[i] != 0 and i < lens-1):   # 找0
-                i += 1
-            while(nums[j] == 0 and j < lens-1):   # 找非0 
-                j += 1
-            # 找到第一个非0 与 0 后交换位置判断  且如果i 小于j才能交换不然就会导致 0跑到左边去
-            if (i < j):
-                nums[i], nums[j] = nums[j], nums[i]
-                if j == lens-1: # 如果是最后一个j那么就break
-                    break  
+class Solution(object):
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        size = len(nums)
+        res = [0] * size # 最终的结果，通过二维数组的index来找到res对应的地方加上贡献 
+        nums = [(nums[i], i) for i in range(size)]    # 二维数组        
+        self.mershsort(nums, 0, size-1, res)
+        return res
+    
+
+# mesh sort 's main function
+    def mershsort(self, nums, low, high, res):
+        if low >= high:   
+            return nums
+        
+        mid = low + (high - low) // 2
+        self.mershsort(nums, low, mid, res)
+        self.mershsort(nums, mid + 1, high, res)
+        return self.mersh(nums, low, mid, high, res) # 全局索引
+    
+    def mersh(self, nums, low, mid, high, res):
+        temp = []         
+        # 使用全局索引所以本行失效i, j, index = 0, 0, 0
+        left = low 
+        right = mid + 1
+        while   left <= mid and right <= high:
+            if nums[left][0] <= nums[right][0]:
+                temp.append(nums[left])
+                res[nums[left][1]] += right - mid - 1 # 对应的原来的index 为 nums[left][1]     i 会发生变换 
+                left += 1  # 同时也要注意先赋值再加一
+                            
             else:
-                if(j < lens-1):
-                    j += 1
-                else:
-                    break # j 超了
-            # 交换完后原来的零位置处是非零的进入后面的循环会发生继续找下一个零 原来是非零的位置处的是0那么就会去找非零的
-            # 所以不用再对i，j加一了 
-            # 但是如果没有发生交换那么就会一直呆着不能下一步
-        print(nums)
+                temp.append(nums[right])
+                right += 1
+        while left <= mid:
+            temp.append(nums[left])
+            res[nums[left][1]] += right - mid - 1
+            left += 1
+        while right <= high:
+            temp.append(nums[right])
+            right += 1            
 
+        nums[low:high + 1] = temp
+        return nums
+
+array = [5,2,6,1]
 a = Solution()
-a.moveZeroes([1])
-
+print(a.countSmaller(array))
