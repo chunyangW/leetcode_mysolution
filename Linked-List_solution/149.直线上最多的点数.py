0961a154f88ce7eaf89e03de9d6828e7811d4543
chunyangW <1790584259@qq.com>
2024-10-12 15:40:35 +0800

    10.12

---
+++ Linked-List_solution/149.直线上最多的点数.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=149 lang=python3
+#
+# [149] 直线上最多的点数
+#
+
+# @lc code=start
+from typing import List
+
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        # for i in range(len)
+            # for j in range(i + 1, len) # 说明前面已经和后面的点连接过统计过了不需要统计
+            
+# @lc code=end
+
